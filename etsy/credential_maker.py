365c465ef4d5d88605286bed05979a3c28c7f987
Paul <siorai@gmail.com>
2016-12-14 20:03:51 -0500

    adding for local credentials file location and checking

--- etsy/credential_maker.py
+++ etsy/credential_maker.py
@@ -3,9 +3,30 @@ from etsy import Etsy
 __author__ = 'siorai@gmail.com (Paul Waldorf)'
 
 
+credentials_dict = {'client_key': '', 
+               'client_secret':'',
+              }
 
+credentials = './credentials'
 
 def initialize():
+  try:
+    f = open(credentials, 'r')
+    print('Found your credentials. Verifying.')
+    f.read()
+    print('Use these settings?')
+    use_em = raw_input('y/n')
+    if use_em == 'n':
+      get_credentials()
+    else:
+      print('Using em!')
+  except IOError:
+    print('Cannot find %s, creating new') % credentials
+    get_credentials()
+  
+
+
+def get_credentials():
   print('Credentials file creation initialized, please input client key:')
   client_key = raw_input('Client Key : ')
   print('Please provide client secret:')
@@ -21,6 +42,10 @@ def verify(client_key, client_secret):
 
   if correct_info == 'y':
     print("Client key is %s and client secret is %s") % (client_key, client_secret)
+    credentials_dict['client_key'] = client_key
+    credentials_dict['client_secret'] = client_secret
+    print("From credentials dict:")
+    print(credentials_dict)
   elif correct_info == 'n':
     initialize()
   else:
